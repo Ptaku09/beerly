d11cf04242e166cc3cf490773fd04b1ccb345871
Mateusz Ptak <ptakm02@gmail.com>
2022-11-02 19:28:31 +0100

    main page - rebuild title area

---
+++ components/molecules/DoubleWaveWrapper.tsx
@@ -0,0 +1,21 @@
+import React, { ReactElement } from 'react';
+import WaveWhite from 'components/atoms/WaveWhite';
+import WaveOrange from 'components/atoms/WaveOrange';
+
+const DoubleWaveWrapper = ({ children }: { children: ReactElement | ReactElement[] }) => {
+  return (
+    <>
+      <div className="relative z-10 lg:-mt-16 2xl:-mt-32 bg-orange-500 flex items-center justify-start flex-col gap-2 pb-7">
+        <div>
+          <WaveWhite />
+        </div>
+        <div className="absolute -bottom-10 md:bottom-0 z-10">{children}</div>
+      </div>
+      <div className="relative z-0 -translate-y-0.5 md:-translate-y-14">
+        <WaveOrange />
+      </div>
+    </>
+  );
+};
+
+export default DoubleWaveWrapper;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import Head from 'next/head';
 import { ReactElement } from 'react';
-import Subtitle from 'components/atoms/Subtitle';
 import Title from 'components/atoms/Title';
-import HomePageBackground from 'components/templates/HomePageBackground';
+import Subtitle from 'components/atoms/Subtitle';
+import DoubleWaveWrapper from 'components/molecules/DoubleWaveWrapper';
 
 export default function Home() {
   return (
@@ -13,20 +13,17 @@ export default function Home() {
       </Head>
 
       <main>
-        <div className="w-screen absolute top-24 2xl:top-36 left-1/2 -translate-x-1/2 z-10 flex items-center justify-center flex-col">
-          <Title>Beerly.</Title>
-          <Subtitle>Your beer library</Subtitle>
-        </div>
+        <DoubleWaveWrapper>
+          <div className="flex justify-center items-center flex-col">
+            <Title>Beerly.</Title>
+            <Subtitle>Your beer library</Subtitle>
+          </div>
+        </DoubleWaveWrapper>
       </main>
     </>
   );
 }
 
 Home.getLayout = function getLayout(page: ReactElement) {
-  return (
-    <>
-      <HomePageBackground />
-      {page}
-    </>
-  );
+  return <>{page}</>;
 };
