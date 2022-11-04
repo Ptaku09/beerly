95aec560cd5e5967bb6b315bed2323d3cba21e15
Mateusz Ptak <ptakm02@gmail.com>
2022-11-04 19:14:25 +0100

    searching - add custom hook for detecting when component lose focus

--- components/molecules/SearchResults.tsx
+++ components/molecules/SearchResults.tsx
@@ -6,7 +6,7 @@ const SearchResults = ({ beers }: { beers: Beer[] }) => {
   return (
     <div
       id="search-results"
-      className="max-h-[50vh] h-fit w-full absolute inset-y-6 -z-10 p-2 bg-slate-100 overflow-y-auto shadow-xl rounded-b-xl border-x-2 border-b-2 transition-all duration-300 hidden peer-focus:block"
+      className="max-h-[50vh] h-fit w-full absolute inset-y-6 -z-10 p-2 bg-slate-100 overflow-y-auto shadow-xl rounded-b-xl border-x-2 border-b-2 transition-all duration-300"
     >
       {beers.map((beer: Beer) => (
         <SearchResultItem key={beer.id} id={beer.id} name={beer.name} imageUrl={beer.image_url} />
--- components/organisms/SearchBar.tsx
+++ components/organisms/SearchBar.tsx
@@ -5,9 +5,11 @@ import { debounce } from 'lodash';
 import { Beer } from 'lib/types';
 import { getBeersByQuery } from 'lib/beers';
 import SearchResults from 'components/molecules/SearchResults';
+import useOnClickOutside from 'hooks/useOnClickOutside';
 
 const SearchBar = () => {
   const [foundedBeers, setFoundedBeers] = useState<Beer[]>([]);
+  const ref = useRef<HTMLDivElement>(null);
   const debouncedSearch = useRef(
     debounce(async (query: string) => {
       if (query.length <= 1) {
@@ -24,16 +26,21 @@ const SearchBar = () => {
     };
   }, [debouncedSearch]);
 
+  useOnClickOutside(ref, () => {
+    setFoundedBeers([]);
+  });
+
   return (
-    <div className="md:w-full relative z-20 mx-4 py-1 flex justify-start items-center font-poppins border-2 rounded-xl">
+    <div ref={ref} className="md:w-full relative z-20 mx-4 py-1 flex justify-start items-center font-poppins border-2 rounded-xl">
       <div className="relative z-20 px-2">
         <Image src={SearchIcon} alt="search icon" width={30} height={30} />
       </div>
       <input
-        className="w-full h-full absolute z-10 pl-11 py-4 rounded-xl outline-orange-500 peer"
+        className="w-full h-full absolute z-10 pl-11 py-4 rounded-xl outline-orange-500"
         type="text"
         maxLength={30}
         onChange={(e: React.ChangeEvent<HTMLInputElement>) => debouncedSearch(e.target.value)}
+        onFocus={(e: React.ChangeEvent<HTMLInputElement>) => debouncedSearch(e.target.value)}
         placeholder="Search..."
       />
       {foundedBeers.length > 0 && <SearchResults beers={foundedBeers} />}
