1c5a31f76df56fa8b7388d3f3d94eb5befa25b93
Mateusz Ptak <ptakm02@gmail.com>
2022-11-05 14:20:18 +0100

    layout - add progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
 import 'styles/globals.css';
+import 'nprogress/nprogress.css';
 import type { AppProps } from 'next/app';
 import { NextPage } from 'next';
-import { ReactElement, ReactNode, useState } from 'react';
+import { ReactElement, ReactNode, useEffect, useState } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import FavoriteBeersProvider from 'providers/FavoriteBeersProvider';
+import NProgress from 'nprogress';
+import { Router } from 'next/router';
 
 export type NextPageWithLayout = NextPage & { getLayout?: (page: ReactElement) => ReactNode };
 type AppPropsWithLayout = AppProps & { Component: NextPageWithLayout };
@@ -11,6 +14,21 @@ type AppPropsWithLayout = AppProps & { Component: NextPageWithLayout };
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const [queryClient] = useState(() => new QueryClient());
   const getLayout = Component.getLayout || ((page: ReactElement) => page);
+
+  useEffect(() => {
+    const handleRouteStart = () => NProgress.start();
+    const handleRouteDone = () => NProgress.done();
+
+    Router.events.on('routeChangeStart', handleRouteStart);
+    Router.events.on('routeChangeComplete', handleRouteDone);
+    Router.events.on('routeChangeError', handleRouteDone);
+
+    return () => {
+      Router.events.off('routeChangeStart', handleRouteStart);
+      Router.events.off('routeChangeComplete', handleRouteDone);
+      Router.events.off('routeChangeError', handleRouteDone);
+    };
+  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
